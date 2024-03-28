3f28ffc6edf51c817c323073d9098d4f7095b69d
VI0L3TF0X <mattex15243@gmail.com>
2024-03-28 14:49:31 +0100

    Add about section, fix printing issues and add section headers

--- src/cv.rs
+++ src/cv.rs
@@ -25,6 +25,7 @@ pub async fn cv_page() -> impl IntoResponse {
     Html(
         CV {
             me: Me::new("https://picsum.photos/200/300", "My name", "My title"),
+            about: "Lorem ipsum dolor sit amet. asdhjkashdkjasdhjkashdk sdas asdas asdsa asd asd asdasdas as asdas adas sas as.<br><br>askldjj l klasjdlk  kjalsl lkasj slkasjd aslkdj alksdj alksjd lkassj  aklsjdl ksjalksj  aksjl jklajld s",
             capabilities: Hexgrid::from_content(vec![
                 Content::Text("Something".into()),
                 Content::Text("Something else".into()),
